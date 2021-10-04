7a1be16dbc83eccfa0260a04a017318eaeec38c1
Mohorima Islam <islammohorima@gmail.com>
2021-10-04 16:31:34 +0600

    mobile menu

---
+++ app.js
@@ -0,0 +1,20 @@
+// reponsive navigation
+
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".nav");
+const navlinks = document.querySelectorAll(".nav ul li");
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  menu.classList.toggle("hide");
+  close.classList.toggle("show");
+});
+navlinks.forEach((el) =>
+  el.addEventListener("click", function (e) {
+    nav.classList.toggle("open");
+    menu.classList.toggle("hide");
+    close.classList.toggle("show");
+  })
+);
